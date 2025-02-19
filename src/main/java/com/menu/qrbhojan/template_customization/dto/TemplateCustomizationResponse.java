56f8d768fdc57dcf93536d554b7cb6336463b008
dahalutsab <dlutsab2120@gmail.com>
2025-02-19 14:54:58 +0545

    save template customization

--- src/main/java/com/menu/qrbhojan/template_customization/dto/TemplateCustomizationResponse.java
+++ src/main/java/com/menu/qrbhojan/template_customization/dto/TemplateCustomizationResponse.java
@@ -1,18 +1,32 @@
 package com.menu.qrbhojan.template_customization.dto;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.menu.qrbhojan.template_customization.entity.TemplateCustomization;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
 public class TemplateCustomizationResponse {
     private Long id;
-    private String customStyles;
+    private Map<String, Object> customStyles;
 
     public TemplateCustomizationResponse(TemplateCustomization user) {
         this.id = user.getId();
-        this.customStyles = user.getCustomStyles();
+        this.customStyles = convertToMap(user.getCustomStyles());
     }
+
+    private Map<String, Object> convertToMap(String customStyles) {
+        try {
+            return new ObjectMapper().readValue(customStyles, Map.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 }
